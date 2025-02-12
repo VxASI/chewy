c59990a4848c7d5e8f8029fd3970a99777a907f6
VxASI <chirag.awale1@gmail.com>
2025-02-12 09:31:00 -0500

    Reapply "test"
    This reverts commit d42deb73b01434d1dba5384236df7190a3409ff8.

--- build/hygiene.js
+++ build/hygiene.js
@@ -15,7 +15,7 @@ const { all, copyrightFilter, unicodeFilter, indentationFilter, tsFormattingFilt
 
 const copyrightHeaderLines = [
 	'/*---------------------------------------------------------------------------------------------',
-	' *  Copyright (c) Microsoft Corporation. All rights reserved.',
+	' *  Copyright (c) Caret Team. All rights reserved.',
 	' *  Licensed under the MIT License. See License.txt in the project root for license information.',
 	' *--------------------------------------------------------------------------------------------*/',
 ];
